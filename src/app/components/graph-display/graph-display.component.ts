9169bf61bee1c0f6776fd8f2d578a9dd7a5606f6
CzarekM2012 <czarekmoczulski@gmail.com>
2022-12-12 00:14:56 +0100

    Drag node on graphDisplay in choose state

--- src/app/components/graph-display/graph-display.component.ts
+++ src/app/components/graph-display/graph-display.component.ts
@@ -30,8 +30,9 @@ export class GraphDisplayComponent implements OnInit, AfterViewInit, OnDestroy {
     worker?: ForceSupervisor, active: FormControl
   } = {active: new FormControl<Boolean>(true, {nonNullable: true})};
   subscriptions: Subscription = new Subscription();
-  // temporarily remembered node, used for adding edges
-  tempNode?: string
+  // temporarily remembered node, used for adding edges and drag
+  tempNode?: string;
+  isDragging: boolean = false;
   refreshSubscription: any;
 
   constructor(private graphStorage: GraphStorageService) {}
@@ -80,7 +81,7 @@ export class GraphDisplayComponent implements OnInit, AfterViewInit, OnDestroy {
       if (this.state.value == 'addNode') {
         const {x, y} = event.event;
         const nodeCoords =
-            this.renderer?.viewportToGraph({x, y}) as Coordinates;
+            this.renderer!.viewportToGraph({x, y}) as Coordinates;
         this.graphStorage.addNode(nodeCoords);
       }
     });
@@ -121,6 +122,34 @@ export class GraphDisplayComponent implements OnInit, AfterViewInit, OnDestroy {
         default:
           break;
       }
+    });
+    this.renderer.on('downNode', (event) => {
+      if (!(this.state.value == 'choose')) return;
+      this.isDragging = true;
+      this.tempNode = event.node;
+      this.graphStorage.graph.setNodeAttribute(
+          this.tempNode, 'highlighted', true);
+      if (!this.renderer!.getCustomBBox())
+        this.renderer!.setCustomBBox(this.renderer!.getBBox());
+    });
+    this.renderer.getMouseCaptor().on('mousemovebody', (event) => {
+      if (!this.isDragging || this.tempNode === undefined) return;
+      const position = this.renderer!.viewportToGraph(event);
+      this.graphStorage.graph.setNodeAttribute(this.tempNode, 'x', position.x);
+      this.graphStorage.graph.setNodeAttribute(this.tempNode, 'y', position.y);
+      event.preventSigmaDefault();
+      event.original.preventDefault();
+      event.original.stopPropagation();
+    });
+    this.renderer.getMouseCaptor().on('mouseup', () => {
+      if (!(this.state.value == 'choose')) return;
+      if (this.tempNode !== undefined) {
+        this.graphStorage.graph.removeNodeAttribute(
+            this.tempNode, 'highlighted');
+      }
+      this.isDragging = false;
+      this.tempNode = undefined;
+      this.renderer!.setCustomBBox(null);
     });
   }
 
